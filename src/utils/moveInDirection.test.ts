69ae90cea420a819ef46ea55d711414be380a34c
Stefan Dzhestanov <stefan.dzhestanov@dojo.tech>
2025-01-27 16:42:21 +0000

    Extracts moveInDirection to utils + unit tests

---
+++ src/utils/moveInDirection.test.ts
@@ -0,0 +1,42 @@
+import moveInDirection from './moveInDirection'
+import { Coordinates, Orientation } from '../types'
+
+describe('moveInDirection', () => {
+  const startPosition: Coordinates = [2, 2]
+
+  describe('when moving from position [2,2]', () => {
+    it('should increase Y when moving North', () => {
+      const result = moveInDirection(startPosition, 'N')
+      expect(result).toEqual([2, 3])
+    })
+
+    it('should decrease Y when moving South', () => {
+      const result = moveInDirection(startPosition, 'S')
+      expect(result).toEqual([2, 1])
+    })
+
+    it('should decrease X when moving East', () => {
+      const result = moveInDirection(startPosition, 'E')
+      expect(result).toEqual([1, 2])
+    })
+
+    it('should increase X when moving West', () => {
+      const result = moveInDirection(startPosition, 'W')
+      expect(result).toEqual([3, 2])
+    })
+  })
+
+  describe('edge cases', () => {
+    it('should handle moving from [0,0]', () => {
+      const origin: Coordinates = [0, 0]
+      expect(moveInDirection(origin, 'N')).toEqual([0, 1])
+      expect(moveInDirection(origin, 'E')).toEqual([-1, 0])
+    })
+
+    it('should handle negative coordinates', () => {
+      const negativePosition: Coordinates = [-1, -1]
+      expect(moveInDirection(negativePosition, 'N')).toEqual([-1, 0])
+      expect(moveInDirection(negativePosition, 'W')).toEqual([0, -1])
+    })
+  })
+})
---
+++ src/utils/moveInDirection.ts
@@ -0,0 +1,18 @@
+import { Coordinates, Orientation } from '../types'
+
+const moveInDirection = ([x, y]: Coordinates, orientation: Orientation): Coordinates => {
+  switch (orientation) {
+    case 'N':
+      return [x, y + 1]
+    case 'S':
+      return [x, y - 1]
+    case 'E':
+      return [x - 1, y]
+    case 'W':
+      return [x + 1, y]
+    default:
+      return [x, y]
+  }
+}
+
+export default moveInDirection
